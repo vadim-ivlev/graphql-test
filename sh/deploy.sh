616b1bdd54e50774bcbf9937bf14351f848d60d7
vadim-ivlev <vadim.ivlev@gmail.com>
2021-02-05 02:54:58 +0300

    .

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 npm run build
-now public
-now alias graphql-test.vadimivlev.now.sh graphql-test
-# now alias https://public.vadimivlev.now.sh graphql-test
+
+# npx vercel public
+# npx vercel alias graphql-test.vadimivlev.now.sh graphql-test
+
+npx vercel --prod public
